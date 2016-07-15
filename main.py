9529112e7a084bada675be2cfbd91ce07c0c4f96
strangecamelcaselogin <strangecamelcaselogin@gmail.com>
2016-07-15 14:54:52 +0400

    New tokinizer and more...

--- main.py
+++ main.py
@@ -1,42 +1,43 @@
-import pymorphy2
 from tokenizer import Tokenizer
 
 
 def load_text(filename, debug=False):
-
     t = Tokenizer()
-    with open(filename, 'r') as f:
-        raw = f.read()
-        # Нужна постобработка
-        # (Склейка устойчивых словосочитаний)
-        word_list = t.get_words2(raw, debug=debug)
 
-    return word_list, raw
+    with open(filename, 'r') as f:
+        text = f.read()
+        token_list = t.get_tokens(text, debug=debug)
+
+    word_list = []
+    for value in token_list:
+        if value.type == 'word' or value.type == 'number':
+            word_list.append(value)
+
+    return text, token_list, word_list
 
 
 def extract_terms(word_list, count=0, debug=False):
 
-    morph = pymorphy2.MorphAnalyzer()
     term_list = dict()
     for word in word_list:
-        t = morph.parse(word)[0]
-        if 'NOUN' in t.tag:
-            if t.normal_form in term_list:
-                term_list[t.normal_form] += 1
+
+        if 'NOUN' in word.tag:
+            if word.normal_form in term_list:
+                term_list[word.normal_form] += 1
             else:
-                term_list[t.normal_form] = 1
+                term_list[word.normal_form] = 1
 
         if debug:
-            print('{} {}:: {}'.format(word, (25 - len(word)) * ' ', t.tag))
+            print('{} {}:: {}'.format(word, (25 - len(word)) * ' ', word.tag))
 
-    term_list = list(reversed(sorted(term_list.items(), key=lambda x: x[1])))
+    term_list = list(reversed(sorted(term_list.items(), key=lambda x: x[1]))) # !!!!!
 
     return term_list if count == 0 else term_list[:count]
 
 
 if __name__ == '__main__':
-    words, _ = load_text('test.txt')
-    print(words)
+    raw, tokens, words = load_text('test.txt')
+    print(*words, sep='\n')
 
     terms = extract_terms(words, count=10)
     print(terms)
--- tokenizer.py
+++ tokenizer.py
@@ -1,75 +1,109 @@
 import re
+import pymorphy2
+import collections
+
+
+cool_tuple = collections.namedtuple('CoolTuple', ('word', 'type', 'tag', 'normal_form', 'score'))
 
 
 class Tokenizer:
-    whitespace = set('()“”\n\t,.;: _/')
-    alphabet = set('ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁйцукенгшщзхъфывапролджэячсмитьбюё№')
+    #
+    #
+    #
+
+    whitespace = set('“”\n\t _/')
+
+    token_type = {'.': 'point',
+                  ',': 'comma',
+                  ';': 'semicolon',
+                  ':': 'colon',
+                  '№': 'num',
+                  '(': 'open_bracket',
+                  ')': 'close_bracket',
+                  '%': 'percent'}
+
+    alphabet = set('ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮЁйцукенгшщзхъфывапролджэячсмитьбюё')
     digits = set('1234567890')
 
-    @staticmethod
-    def get_words(text):
-        # Получаем лексемы без пробелов, запятых, точек
-        # Кривой способ, пусть будет
-
-        words = []
-        word = ''
-        while text != '':
-            v = text[0]
-            if v in Tokenizer.whitespace:
-                if word != '':
-                    words.append(word)
-                    word = ''
-
-            else:
-                word += v
-
-            text = text[1:]
-
-        if word != '':
-            words.append(word)
-
-        return words
+    morph = pymorphy2.MorphAnalyzer()
 
     @staticmethod
-    def get_words2(text, debug=False):
-        # Получаем лексемы без пробелов, запятых, точек
+    def append_token(lst, elem, type_):
+        t = Tokenizer.morph.parse(elem)[0]
+        lst.append(cool_tuple(elem, type_, t.tag, t.normal_form, t.score))
 
-        words = []
+    @staticmethod
+    def get_tokens(text, debug=False):
+        #  Делит текст на токены, определяет нормальную форму,
+        #  а также часть речи, род, падеж, число
+        #
+
+        tokens = []
         state = 0
-        word = ''
-        for i, v in enumerate(text):
+        token = ''
+        for index, value in enumerate(text):
             if state == 0:
-                if word != '':
-                    words.append(word)
-                    word = ''
+                if token != '' and token not in Tokenizer.whitespace:
+                    if token in Tokenizer.token_type:
+                        Tokenizer.append_token(tokens, token, Tokenizer.token_type[token])
 
-                if v in Tokenizer.alphabet:
+                    else:
+                        Tokenizer.append_token(tokens, token, 'Unk')
+
+                token = value
+                if value in Tokenizer.alphabet:
                     state = 1
-                    word = v
 
-                elif v in Tokenizer.digits:
+                elif value in Tokenizer.digits:
+                    state = 3
+
+            elif state == 1:  # Очередная буква в слове
+                if value in Tokenizer.alphabet:
+                    token += value
+
+                elif value == '-':
+                    token += value
                     state = 2
-                    word = v
 
-            elif state == 1:
-                if v in Tokenizer.alphabet:
-                    word += v
-
-                elif v in Tokenizer.whitespace:
+                else:
+                    Tokenizer.append_token(tokens, token, 'word')
+                    token = value
                     state = 0
 
-            elif state == 2:
-                if v in Tokenizer.digits or v == '.':
-                    word += v
+            elif state == 2:  # Очередная буква после дефиса
+                if value in Tokenizer.alphabet:
+                    token += value
 
-                elif v in Tokenizer.whitespace:
+                else:
+                    Tokenizer.append_token(tokens, token, 'word')
+                    token = value
+                    state = 0
+
+            elif state == 3:  # Очередная цифра
+                if value in Tokenizer.digits:
+                    token += value
+
+                elif value == '.':
+                    token += value
+                    state = 4
+
+                else:
+                    Tokenizer.append_token(tokens, token, 'number')
+                    token = value
+                    state = 0
+
+            elif state == 4:  # Очередная цифра после точки
+                if value in Tokenizer.digits:
+                    token += value
+
+                else:
+                    Tokenizer.append_token(tokens, token, 'number')
+                    token = value
                     state = 0
 
             if debug:
-                print('{} - {} === {}'.format(i, v, state))
+                print(state, token)
 
-        return words
+        Tokenizer.append_token(tokens, '#', 'end')
 
-    @staticmethod
-    def tokens(text):
-        pass
+        return tokens
